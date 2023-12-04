c9b4dc92ef50afa6354e48bd7f050b5c4aba8d1a
ykucukcinar <yigitkucukcinar@hotmail.com>
2023-12-04 21:34:13 +0300

    Add request manager

---
+++ app/src/main/java/com/kucukcinar/recipeapp/RequestManager.java
@@ -0,0 +1,29 @@
+package com.kucukcinar.recipeapp;
+
+import android.content.Context;
+import com.kucukcinar.recipeapp.models.RandomRecipeApiResponse;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public class RequestManager {
+    Context context;
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://api.spoonacular.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+    public RequestManager(Context context) {
+        this.context = context;
+    }
+
+    private interface CallRandomRecipes{
+        @GET("recipes/random")
+        Call<RandomRecipeApiResponse> callRandomRecipe(
+                @Query("apiKey") String apiKey,
+                @Query("number") String number
+        );
+    }
+}
