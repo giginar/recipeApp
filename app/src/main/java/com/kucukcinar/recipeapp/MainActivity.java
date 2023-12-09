d3a64471e8437701089d0972e9eb9d5900eae74f
ykucukcinar <yigitkucukcinar@hotmail.com>
2023-12-10 00:19:55 +0300

    Upgrade MainActivity

--- app/src/main/java/com/kucukcinar/recipeapp/MainActivity.java
+++ app/src/main/java/com/kucukcinar/recipeapp/MainActivity.java
@@ -1,13 +1,49 @@
 package com.kucukcinar.recipeapp;
 
+import android.app.ProgressDialog;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.kucukcinar.recipeapp.adapters.RandomRecipeAdapter;
+import com.kucukcinar.recipeapp.listeners.RandomRecipeResponseListener;
+import com.kucukcinar.recipeapp.models.RandomRecipeApiResponse;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressDialog dialog;
+    RequestManager manager;
+    RandomRecipeAdapter randomRecipeAdapter;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dialog = new ProgressDialog(this);
+        dialog.setTitle("Loading...");
+
+        manager = new RequestManager(this);
+        manager.getRandomRecipes(randomRecipeResponseListener);
+        dialog.show();
     }
+
+    private final RandomRecipeResponseListener randomRecipeResponseListener = new RandomRecipeResponseListener() {
+        @Override
+        public void didFetch(RandomRecipeApiResponse response, String message) {
+            dialog.dismiss();
+            recyclerView = findViewById(R.id.recycler_random);
+            recyclerView.setHasFixedSize(true);
+            recyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this, 1));
+            randomRecipeAdapter = new RandomRecipeAdapter(MainActivity.this, response.recipes);
+            recyclerView.setAdapter(randomRecipeAdapter);
+        }
+
+        @Override
+        public void didError(String message) {
+            Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+        }
+    };
 }
