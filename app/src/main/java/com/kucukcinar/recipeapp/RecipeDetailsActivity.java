fe703c7389148de72117c48b8784c33a3aebb92d
ykucukcinar <yigitkucukcinar@hotmail.com>
2023-12-19 21:28:47 +0300

    Add Recipe Details Layout

--- app/src/main/java/com/kucukcinar/recipeapp/RecipeDetailsActivity.java
+++ app/src/main/java/com/kucukcinar/recipeapp/RecipeDetailsActivity.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class RecipeDetailsActivity extends AppCompatActivity {
-
+    int id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_details);
+
+        //TODO
+
+        id = Integer.parseInt(getIntent().getStringExtra("id"));
     }
 }
